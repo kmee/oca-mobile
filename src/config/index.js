9dcc0c8bd2f81eb044f3bd8093d87142b68c0852
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-09 19:49:24 -0300

    [IMP] Env files
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+import {
+  APP_NAME,
+  APP_SITE,
+  APP_SITE_TITLE,
+  DB_FILTER,
+  DB_NAME,
+} from 'react-native-dotenv';
+
+export default {
+  APP_NAME,
+  APP_SITE,
+  APP_SITE_TITLE,
+  DB_FILTER,
+  DB_NAME,
+};
