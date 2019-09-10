ed0db071583d34050ac99b460b6648e2096f8ffe
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-10 19:30:41 -0300

    [IMP] Backend integration
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- src/pages/backend/index.js
+++ src/pages/backend/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-import {ActivityIndicator, AsyncStorage, StyleSheet} from 'react-native';
+import {ActivityIndicator, AsyncStorage, Vibration} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 import styles from './style';
+
+const DURATION = 10000;
+const PATTERN = [1000, 2000, 3000, 4000];
 
 export default class OdooBackend extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  StartVibrationFunction = () => {
+    // Device Will Vibrate for 10 seconds.
+    // Vibration.vibrate(DURATION);
+
+    // Android Device Will Vibrate in pattern : Wait 1sec -> vibrate 2sec -> wait 3sec.
+    // iOS Device Will Vibrate in pattern : Wait 1sec -> Vibrate -> wait 2sec -> Vibrate -> wait 3sec -> Vibrate
+
+    Vibration.vibrate(PATTERN);
+
+    // Android Device Will Vibrate in above pattern Infinite Time.
+    // iOS Device Will Vibrate in above pattern Infinite Time.
+
+    // Vibration.vibrate(PATTERN, true)
+    // Vibration.cancel();
+  };
 
   async componentDidMount() {
     var url = await AsyncStorage.getItem('last_url');
@@ -19,16 +42,25 @@ export default class OdooBackend extends React.Component {
     this.setState({url: url});
   }
 
-  // componentDidMount() {
-  //   console.log('Componentem montou');
-  // }
-
   _onNavigationStateChange(webViewState) {
     AsyncStorage.setItem('last_url', webViewState.url);
   }
 
+  _backendMessage(message) {
+    switch (message) {
+      case 'REACT_EXIT':
+        this.StartVibrationFunction();
+        this.props.navigation.navigate('Home');
+        break;
+      case 'OTHER_MESSAGE':
+        this.props.navigation.navigate('Home');
+        break;
+      default:
+        console.log(message);
+    }
+  }
+
   ActivityIndicatorLoadingView() {
-    //making a view to show to while loading the webpage
     return (
       <ActivityIndicator
         color="#009688"
@@ -39,15 +71,28 @@ export default class OdooBackend extends React.Component {
   }
 
   render() {
+    const runFirst = `
+      $("[data-menu=logout]").attr('data-menu','logout_custom');
+      $("[data-menu=logout_custom]").click(()=>{
+          window.ReactNativeWebView.postMessage("REACT_EXIT");
+      });
+    `;
+
+    console.log(this.state.cookie);
+
     return (
       <WebView
         source={{uri: this.state.url}}
         onNavigationStateChange={this._onNavigationStateChange.bind(this)}
         javaScriptEnabled={true}
         domStorageEnabled={true}
-        injectedJavaScript={this.state.cookie}
         startInLoadingState={true}
         renderLoading={this.ActivityIndicatorLoadingView}
+        injectedJavaScript={runFirst}
+        // injectedJavaScript={this.state.cookie}
+        onMessage={event => {
+          this._backendMessage(event.nativeEvent.data);
+        }}
       />
     );
   }
