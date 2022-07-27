1d5d82dc034703d59f587639407d4583eb4257fb
Abner Rodrigues <abnerluis1001@gmail.com>
2022-07-27 11:37:53 -0300

    Major refactor to use function components and hooks

--- src/pages/backend/index.js
+++ src/pages/backend/index.js
@@ -6,36 +6,38 @@ import {WebView} from 'react-native-webview';
 
 import styles from './style';
 
-const DURATION = 10000;
 const PATTERN = [1000, 2000, 3000, 4000];
 
-export default class OdooBackend extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function ActivityIndicatorLoadingView() {
+  return (
+    <ActivityIndicator
+      color="#009688"
+      size="large"
+      style={styles.ActivityIndicatorStyle}
+    />
+  );
+}
 
-  static navigationOptions = {
-    headerShown: false,
-  };
+export default function OdooBackend({navigation}) {
+  const [url, setUrl] = React.useState({});
 
-  StartVibrationFunction = () => {
-    // Device Will Vibrate for 10 seconds.
-    // Vibration.vibrate(DURATION);
+  // const StartVibrationFunction = () => {
+  //   // Device Will Vibrate for 10 seconds.
+  //   // Vibration.vibrate(DURATION);
 
-    // Android Device Will Vibrate in pattern : Wait 1sec -> vibrate 2sec -> wait 3sec.
-    // iOS Device Will Vibrate in pattern : Wait 1sec -> Vibrate -> wait 2sec -> Vibrate -> wait 3sec -> Vibrate
+  //   // Android Device Will Vibrate in pattern : Wait 1sec -> vibrate 2sec -> wait 3sec.
+  //   // iOS Device Will Vibrate in pattern : Wait 1sec -> Vibrate -> wait 2sec -> Vibrate -> wait 3sec -> Vibrate
 
-    Vibration.vibrate(PATTERN);
+  //   Vibration.vibrate(PATTERN);
 
-    // Android Device Will Vibrate in above pattern Infinite Time.
-    // iOS Device Will Vibrate in above pattern Infinite Time.
+  //   // Android Device Will Vibrate in above pattern Infinite Time.
+  //   // iOS Device Will Vibrate in above pattern Infinite Time.
 
-    // Vibration.vibrate(PATTERN, true)
-    // Vibration.cancel();
-  };
+  //   // Vibration.vibrate(PATTERN, true)
+  //   // Vibration.cancel();
+  // };
 
-  async componentDidMount() {
+  React.useEffect(() => {
     AsyncStorage.getItem('last_url').then(last_url => {
       if (
         last_url === 'about:blank' ||
@@ -43,72 +45,58 @@ export default class OdooBackend extends React.Component {
         last_url == null
       ) {
         AsyncStorage.getItem('server_backend_url')
-          .then(url => {
-            if (url) {
-              this.setState({url: url});
+          .then(uri => {
+            if (uri) {
+              setUrl(uri);
             }
           })
           .catch(e => {
             console.log(e);
           });
       } else {
-        this.setState({url: last_url});
+        setUrl(last_url);
       }
     });
-  }
+  }, []);
 
-  _onNavigationStateChange(webViewState) {
+  const onNavigationStateChange = webViewState => {
     AsyncStorage.setItem('last_url', webViewState.url);
-  }
+  };
 
-  _backendMessage(message) {
+  const handleBackendMessage = message => {
     switch (message) {
       case 'REACT_EXIT':
         // this.StartVibrationFunction();
-        this.props.navigation.navigate('Home');
+        navigation.navigate('Home');
         break;
       case 'OTHER_MESSAGE':
-        this.props.navigation.navigate('Home');
+        navigation.navigate('Home');
         break;
       default:
         console.log(message);
     }
-  }
+  };
 
-  ActivityIndicatorLoadingView() {
-    return (
-      <ActivityIndicator
-        color="#009688"
-        size="large"
-        style={styles.ActivityIndicatorStyle}
-      />
-    );
-  }
-
-  render() {
-    const runFirst = `
+  const runFirst = `
       $("[data-menu=logout]").attr('data-menu','logout_custom');
       $("[data-menu=logout_custom]").click(()=>{
           window.ReactNativeWebView.postMessage("REACT_EXIT");
       });
     `;
 
-    console.log(this.state.cookie);
-
-    return (
-      <WebView
-        source={{uri: this.state.url}}
-        onNavigationStateChange={this._onNavigationStateChange.bind(this)}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        renderLoading={this.ActivityIndicatorLoadingView}
-        injectedJavaScript={runFirst}
-        // injectedJavaScript={this.state.cookie}
-        onMessage={event => {
-          this._backendMessage(event.nativeEvent.data);
-        }}
-      />
-    );
-  }
+  return (
+    <WebView
+      source={url}
+      onNavigationStateChange={onNavigationStateChange}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      startInLoadingState={true}
+      renderLoading={ActivityIndicatorLoadingView}
+      injectedJavaScript={runFirst}
+      // injectedJavaScript={this.state.cookie}
+      onMessage={event => {
+        handleBackendMessage(event.nativeEvent.data);
+      }}
+    />
+  );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,67 +6,62 @@ import {Button, Card, Icon, Text} from 'react-native-elements';
 
 import styles from './style';
 
-export default class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user_name: ' ',
-      server: ' ',
-      db: ' ',
-      image_small: '',
-    };
-  }
+export default function HomeScreen({navigation}) {
+  const [state, setState] = React.useState({
+    user_name: ' ',
+    server: ' ',
+    db: ' ',
+    image_small: '',
+  });
 
-  static navigationOptions = {
-    headerShown: false,
+  const showMoreApp = () => {
+    navigation.navigate('Backend');
   };
 
-  async componentDidMount() {
-    var server = await AsyncStorage.getItem('server_backend_url');
-    var user_name = await AsyncStorage.getItem('user_display_name');
-    var image_small = await AsyncStorage.getItem('image_small');
-    var db = await AsyncStorage.getItem('database');
-    this.setState({
-      server: server,
-      user_name: user_name,
-      db: db,
-      image_small: image_small,
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Card title={this.state.user_name}>
-          <Image
-            style={styles.image_badge}
-            source={{uri: `data:image;base64,${this.state.image_small}`}}
-          />
-          <Text style={styles.text_badge}>{this.state.server}</Text>
-          <Text style={styles.text_badge}>Database: {this.state.db}</Text>
-          <Button
-            title="Backend"
-            onPress={this._showMoreApp}
-            buttonStyle={styles.loginButton}
-            icon={<Icon name="home" color="#ffffff" />}
-          />
-          <Button
-            title="Logout"
-            onPress={this._signOutAsync}
-            buttonStyle={styles.loginButton}
-            icon={<Icon name="exit-to-app" color="#ffffff" />}
-          />
-        </Card>
-      </View>
-    );
-  }
-
-  _showMoreApp = () => {
-    this.props.navigation.navigate('Other');
-  };
-
-  _signOutAsync = async () => {
+  const signOutAsync = async () => {
     await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
+    navigation.navigate('SignIn');
   };
+
+  React.useEffect(() => {
+    const bootstrap = async () => {
+      const server = await AsyncStorage.getItem('server_backend_url');
+      const user_name = await AsyncStorage.getItem('user_display_name');
+      const image_small = await AsyncStorage.getItem('image_small');
+      const db = await AsyncStorage.getItem('database');
+
+      setState({
+        server: server,
+        user_name: user_name,
+        db: db,
+        image_small: image_small,
+      });
+    };
+    bootstrap();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Card title={state.user_name}>
+        <Image
+          style={styles.image_badge}
+          source={{uri: `data:image;base64,${state.image_small}`}}
+        />
+        <Text style={styles.text_badge}>{state.server}</Text>
+        <Text style={styles.text_badge}>Database: {state.db}</Text>
+        <Button
+          title="Backend"
+          onPress={showMoreApp}
+          buttonStyle={styles.loginButton}
+          icon={<Icon name="home" color="#ffffff" />}
+        />
+        <Button
+          title="Logout"
+          onPress={signOutAsync}
+          buttonStyle={styles.loginButton}
+          icon={<Icon name="exit-to-app" color="#ffffff" />}
+        />
+      </Card>
+    </View>
+  );
 }
